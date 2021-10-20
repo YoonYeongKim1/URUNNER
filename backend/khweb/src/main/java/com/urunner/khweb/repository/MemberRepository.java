fecaca8f6e837fd27457d4c26d34b2d405b930d4
YoonYeongKim1 <03y20y@naver.com>
2021-10-20 21:32:00 +0900

    왜죠

--- backend/khweb/src/main/java/com/urunner/khweb/repository/MemberRepository.java
+++ backend/khweb/src/main/java/com/urunner/khweb/repository/MemberRepository.java
@@ -1,6 +1,6 @@
 package com.urunner.khweb.repository;
 
-import com.urunner.khweb.entity.member.Member;
+import com.urunner.khweb.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
