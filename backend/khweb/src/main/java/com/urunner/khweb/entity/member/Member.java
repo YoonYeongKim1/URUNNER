7c5d8ee27ee4275c97577859994d768a4e70d1f9
YoonYeongKim1 <03y20y@naver.com>
2021-10-25 16:39:14 +0900

    d

--- backend/khweb/src/main/java/com/urunner/khweb/entity/member/Member.java
+++ backend/khweb/src/main/java/com/urunner/khweb/entity/member/Member.java
@@ -8,7 +8,6 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Date;
 import java.util.List;
-
 @NoArgsConstructor
 @Getter
 @Entity
@@ -18,43 +17,44 @@ public class Member {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "member_no")
     private Long memberNo;
-
     private String email;
     private String name;
     private String password;
+    private String introduce;
 
     @CreationTimestamp
     private Date regDate;
-
     @UpdateTimestamp
     private Date upDate;
 
 
-    public Member(String email, String password, String name) {
+    public Member(String email, String password, String name, String introduce) {
         this.email = email;
         this.password = password;
         this.name = name;
+        this.introduce = introduce;
     }
 
     @OneToMany(cascade = CascadeType.ALL)
     @JoinColumn(name = "member_no")
-    private Collection<Role> roles;
+    private Collection<Role> roles = new ArrayList<>();
 
     public void setEmail(String email) {
         this.email = email;
     }
-
     public void setName(String name) {
         this.name = name;
     }
-
     public void setPassword(String password) {
         this.password = password;
     }
-
-
     public void setRoles(Role role) {
         this.roles.add(role);
     }
 
-}
+    public void setIntroduce(String introduce) {
+        this.introduce = introduce;
+    }
+
+
+} 
